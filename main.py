70ea485e47ef82b0fe8c0be9311154478e2d992d
boa <boa@protonmail.com>
2023-11-29 09:49:23 +0100

    add new endpoints

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+import yaml
 from fastapi import FastAPI
 from models import Session, Article
 from apscheduler.schedulers.background import BackgroundScheduler
 from rss_parser import fetch_and_store_feeds
 
-fetch_and_store_feeds()
+with open('scraping_rules.yaml', 'r') as file:
+    scraping_rules = yaml.safe_load(file)
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(fetch_and_store_feeds, 'interval', minutes=1)
+scheduler.add_job(fetch_and_store_feeds, 'interval', hours=1)
 scheduler.start()
 
 app = FastAPI()
 
 @app.get("/articles/")
-def read_articles():
+def read_all_articles():
     session = Session()
     articles = session.query(Article).all()
     return articles
+
+@app.get("/articles/{feed}")
+def read_articles(feed):
+    session = Session()
+    articles = session.query(Article).filter(Article.feed == feed).all()
+    return articles
+
+@app.get("/feeds/")
+def read_rules():
+    return [r for r in scraping_rules]
 
